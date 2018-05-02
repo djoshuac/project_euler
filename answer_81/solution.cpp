54b548bb58fc1bf3ed050144893702359ecb5977
djoshuac <21327729+djoshuac@users.noreply.github.com>
2018-05-01 21:52:49 -0600

    min, gotta read better

--- answer_81/solution.cpp
+++ answer_81/solution.cpp
@@ -17,18 +17,16 @@ int main() {
     }
   }
 
-  vector<ll> prev(n, 0l);
   vector<ll> dp(n, 0l);
-  prev[0] = grid[0][0];
+  dp[0] = grid[0][0];
   for (int j = 1; j < n; j++) {
-    prev[j] = grid[0][j-1] + grid[0][j];
+    dp[j] = grid[0][j-1] + grid[0][j];
   }
   for (int i = 1; i < n; i++) {
-    dp[0] = prev[0] + grid[i][0];
+    dp[0] = dp[0] + grid[i][0];
     for (int j = 1; j < n; j++) {
-      dp[j] = max(prev[j-1], prev[j]) + grid[i][j];
+      dp[j] = min(dp[j-1], dp[j]) + grid[i][j];
     }
-    prev = dp;
   }
 
   cout << dp[n-1] << endl;
